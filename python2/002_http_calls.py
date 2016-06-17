25169da281f58853180cd8a0386077f5261f1c9b
iStormy <truecoderhd@gmail.com>
2016-06-17 11:10:39 -0400

    Update 002_http_calls.py

--- python2/002_http_calls.py
+++ python2/002_http_calls.py
@@ -1,20 +1,30 @@
 #!/bin/env python2.7
 
-import urllib2
-# This is an import. Python has a very large built-in library, providing hundreds of thousands of useful functions and objects. In Python,
-# you import "modules." Modules can have functions and variables defined within them directly or have sub objects inside them.  You can also download
-# and install additional modules, created by people other than the Python maintainers. Check out the Python Package Index (or pypi) for more info.
-# The urllib2 module is one of the modules used for making web/http requests.
+# Single line comment 
+"""
+Multiline 
+comment.
+"""
 
-opener = urllib2.build_opener()
-# This is an example of calling a function defined within a module. Specifically, the build_opener() function returns an object that allows us to make
-# http requests. When a function "returns" something, that means it is outputting that thing. The output is a value of some sort. It can be stored
-# in a variable, fed as a parameter to another function, and anything else that you can do with values. In this case, we're storing the opener that
-# the build_opener() function returns into a variable we're calling "opener."
+import requests
 
-response = opener.open("http://google.com")
-response_content = response.read()
-print("response:")
-print(response_content)
+"""
+This is an import. Python has a very large built-in library, providing hundreds of thousands of useful functions and objects. In Python,
+you import "modules." Modules can have functions and variables defined within them directly or have sub objects inside them.  You can also download
+and install additional modules, created by people other than the Python maintainers. Check out the Python Package Index (or pypi) for more info.
+The "requests" is one of the modules used for making web/http requests. This module was built using python's builtin http/web reques modules.
+"""
+
+response = requests.get("https://google.com")
+print("Response: %s" %s response.content) # response.text would work too.
+
+"""
+ I don't know how you're gonna explain the code above but try
+ to use it as something to base it off of. Also I recommend, 
+ guiding users through the process of setting up the environment
+ in your readme.md
+"""
 
 # Incomplete. Next, we'll cover wtf the code above means.
+
+# sorry if I rip'd what you were trying to do. I'll add more once I am home.
